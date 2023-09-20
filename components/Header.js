2b05d6b3cec3d9b7781d5a6717538332bd34f69b
Nuno Marques <ositaka@gmail.com>
2023-09-20 23:11:01 +0200

    New menu style

--- components/Header.js
+++ components/Header.js
@@ -8,39 +8,32 @@ export default function Header({ }) {
 
   menu = menu.section
 
-  // const myFunction = () => {
-  //   var x = document.getElementById("myTopnav");
-  //   if (x.className === "navigation") {
-  //     x.className += " responsive";
-  //   } else {
-  //     x.className = "navigation";
-  //   }
-  // }
-
   return (<>
 
     <header className="header">
-      <div className="navigation" id="myTopnav">
-        <Link href="/#Home" className='active'>Soul Healing</Link>
+      <div className="header__wrapper">
+        <Link href="/#Home">Soul Healing</Link>
 
-        {menu
-          .filter(section => section.main_menu.show)
-          .map(section => {
-            return (<Link key={section.main_menu.name} href={'/#' + section.main_menu.name} className=''>
-              {section.main_menu.name}
-            </Link>
-            )
-          })}
-        {/* <Link href="/bookings" className=''>Bookings</Link> */}
-        <Link href="/news" className=''>News</Link>
-        {/* <Link href="/contact" className=''>Contact</Link> */}
-        <a href="javascript:void(0);" className="icon">
-          <div className="icon__menu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </a>
+        <div className="navigation">
+          {menu
+            .filter(section => section.main_menu.show)
+            .map(section => {
+              return (<Link key={section.main_menu.name} href={'/#' + section.main_menu.name} className=''>
+                {section.main_menu.name}
+              </Link>
+              )
+            })}
+          {/* <Link href="/bookings" className=''>Bookings</Link> */}
+          <Link href="/news" className=''>News</Link>
+          {/* <Link href="/contact" className=''>Contact</Link> */}
+          <a href="javascript:void(0);" className="icon">
+            <div className="icon__menu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </a>
+        </div>
       </div>
     </header>
   </>
